faa04cab5fe2fc0a8c10bd27dca5bf31f6c79efe
John Ludlow <john.ludlow@halfords.co.uk>
2025-11-04 21:42:49 +0000

    Add basic ECS classes - resolve conflicts - add child controls back in

--- Local/UserInterface/ImGuiSample/UserInterface/Controls/TestWindow.cs
+++ Local/UserInterface/ImGuiSample/UserInterface/Controls/TestWindow.cs
@@ -4,8 +4,8 @@ public class TestWindow : BaseWindow
 {
     public TestWindow() : base("Test Window")
     {
-        Controls.Add(new ImGuiPanel("Panel 1"));
-        Controls.Add(new ImGuiPanel("Panel 2"));
-        Controls.Add(new ImGuiPanel("Panel 3"));
+        Controls.Add(new ImGuiPanel("Panel 1") { Controls = { new TextControl { Text = "This is some text inside Panel 1." } } });
+        Controls.Add(new ImGuiPanel("Panel 2") { Controls = { new TextControl { Text = "This is some text inside Panel 2." } } });
+        Controls.Add(new ImGuiPanel("Panel 3") { Controls = { new TextControl { Text = "This is some text inside Panel 3." } } });
     }
 }
